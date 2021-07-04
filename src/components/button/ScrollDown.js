69d8d2557fe713e2e6734c52f938667abdc97112
kanmaytacker <tanmay@locus.sh>
2021-07-04 21:39:18 +0530

    Adds smooth scrolling.

--- src/components/button/ScrollDown.js
+++ src/components/button/ScrollDown.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import 'twin.macro'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const ScrollDown = ({ disabled, to }) => {
   return disabled ? null : (
-    <a
+    <AnchorLink
       href={to}
       aria-label='Scroll down'
       tw='flex items-center justify-center w-10 h-10 mx-auto text-gray-600 duration-500 transform border border-gray-400 rounded-full hover:text-teal-accent-700 hover:border-teal-accent-700 hover:shadow hover:scale-110 animate-bounce'
@@ -17,7 +18,7 @@ const ScrollDown = ({ disabled, to }) => {
       >
         <path d='M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z' />
       </svg>
-    </a>
+    </AnchorLink>
   )
 }
 
